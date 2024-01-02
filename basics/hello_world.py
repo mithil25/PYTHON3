a9988ea3dc7f847c38006bc1b35ed376e3182e4d
work <mithil.anchan@cogoport.com>
2024-01-02 23:37:07 +0530

    done with numbers

---
+++ basics/hello_world.py
@@ -0,0 +1,8 @@
+def main():
+    print("Hello World!")
+    print("Good Bye!")
+def print_my_name(name = "Ashu"):
+    print(f'My name is {name}')
+if __name__ == "__main__":
+    main()
+    print_my_name()
---
+++ basics/numbers.py
@@ -0,0 +1,28 @@
+'''
+ Numbers are basically 3 types in Python
+ Integer class type int
+ Floating point class type float
+ Complex Number class type complex
+ Here number is a primitive data type whenever we assign a number,
+ it creates a new instance of number depending on the type (int,float,complex).
+'''
+# Int + Int = Integer
+# Int + Float = Float
+# Int - Float = Float
+# Int * Float = Float
+# Int / Int = Float
+# Float // Float = Float
+# Int / Float = Float
+# Int // Float = Float
+# Float ** Int = Float (** is power function in python)
+# Int ** Float = Int
+print(1+1)
+print(1+2.3)
+print(1-2.0)
+print(2*1.5)
+print(2.3/2.0)
+print(2.1//2.0)
+print(9 / 2.0)
+print(9 // 2.0)
+print(2.3 ** 2)
+print(4 ** (1/2))
